46edc859aa8e21d3c66eadfebeffe655ed0665ab
Rodri207 <rodrigosantospais@gmail.com>
2024-02-05 11:34:42 +0000

    atualizar

--- projeto_final_13/templates/Articles/view.php
+++ projeto_final_13/templates/Articles/view.php
@@ -1,6 +1,7 @@
 <html></html>
 <!-- File: Templates/Articles/view.php -->
-<h1><?= h($article->title) ?></h1>
+<h1><?= h($article->Nome) ?></h1>
+<h1><?= h($article->Posição) ?></h1>
 <p><?= h($article->body) ?></p>
 <p><small>Criado: <?= $article->created->format(DATE_RFC850) ?></small></p>
 </html>
