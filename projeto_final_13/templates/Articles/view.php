575a745e51942d67e59bf8ab5a195d9301247ce2
Rodri207 <rodrigosantospais@gmail.com>
2024-02-08 11:14:46 +0000

    Update view.php

--- projeto_final_13/templates/Articles/view.php
+++ projeto_final_13/templates/Articles/view.php
@@ -3,5 +3,4 @@
 <h1><?= h($article->Nome) ?></h1>
 <h1><?= h($article->Posição) ?></h1>
 <p><?= h($article->body) ?></p>
-<p><small>Criado: <?= $article->created->format(DATE_RFC850) ?></small></p>
 </html>
